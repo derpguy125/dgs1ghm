33a159c025c2611c63ec011cb932a1c44a15afd9
sonicthedutchhedgehog <69474032+sonicthedutchhedgehog@users.noreply.github.com>
2021-08-17 22:06:49 +0200

    calm

--- sonic1.asm
+++ sonic1.asm
@@ -37,8 +37,8 @@ Vectors:	dc.l $FFFE00, EntryPoint, BusError, AddressError
 		dc.l ErrorTrap,	ErrorTrap, ErrorTrap, ErrorTrap
 Console:	dc.b 'SEGA MEGA DRIVE ' ; Hardware system ID
 Date:		dc.b '(C)SEGA 1991.APR' ; Release date
-Title_Local:	dc.b 'SONIC THE HEDGEHOG GITHUB MADNESS               ' ; Domestic name
-Title_Int:	dc.b 'SONIC THE HEDGEHOG GITHUB MADNESS               ' ; International name
+Title_Local:	dc.b 'SONIC THE HEDGEHOG GITHUB CALMNESS              ' ; Domestic name
+Title_Int:	dc.b 'SONIC THE HEDGEHOG GITHUB CALMNESS             ' ; International name
 Serial:		dc.b 'GM 00001009-00'   ; Serial/version number
 Checksum:	dc.w 0
 		dc.b 'J               ' ; I/O support
